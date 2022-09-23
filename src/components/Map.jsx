6bf9b73047668d853c601ab6eef262426844c5f3
Blairi <axel.avi.fer@gmail.com>
2022-09-23 11:23:24 -0500

    zoom and text pop up

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -22,14 +22,14 @@ export const Map = () => {
   
 
   return (
-    <MapContainer center={ position } zoom={13} scrollWheelZoom={true} zoomControl={false}>
+    <MapContainer center={ position } zoom={15} scrollWheelZoom={true} zoomControl={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={ position }>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          { ipAddress?.isp }
         </Popup>
       </Marker>
       <ChangeView pos={ position }/>
