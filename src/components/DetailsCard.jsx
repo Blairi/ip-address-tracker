0735ac168cbcbe7d85923cefb8055694fc6ec4e3
Blairi <axel.avi.fer@gmail.com>
2022-09-23 11:21:35 -0500

    using context hook

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { IpAddressContext } from "../context/IpAddressContext"
 
-export const DetailsCard = ({ip = '', location = {}, isp = ''}) => {
+export const DetailsCard = () => {
 
   const { ipAddress } = useContext( IpAddressContext );
 
@@ -9,30 +9,41 @@ export const DetailsCard = ({ip = '', location = {}, isp = ''}) => {
     <div className='bg-white rounded-lg flex flex-col items-center gap-3 py-5'>
 
       {
-        ipAddress?.isLoading 
+        ipAddress?.error && (
+          <p className="font-bold text-red-500 text-lg">{ipAddress?.errorMessage}</p>
+        )
+      }
+
+      {
+
+        ipAddress?.isLoading
         ? (
           <div className="lds-dual-ring"></div>
         )
         : (
+          !ipAddress?.error 
+          &&
           <>
             <div>
               <h3 className='text-xs text-[#969696] text-center'>IP ADDRESS</h3>
-              <span className='text-lg font-bold text-[#2b2b2b]'>{ip}</span>
+              <span className='text-lg font-bold text-[#2b2b2b]'>{ipAddress?.ip}</span>
             </div>
 
             <div>
               <h3 className='text-xs text-[#969696] text-center'>LOCATION</h3>
-              <span className='text-lg font-bold text-[#2b2b2b]'>{location.city}, {location.region}, {location.postalCode}</span>
+              <span className='text-lg font-bold text-[#2b2b2b]'>
+                {ipAddress?.location.city}, {ipAddress?.location.region}, {ipAddress?.location.postalCode}
+              </span>
             </div>
 
             <div>
               <h3 className='text-xs text-[#969696] text-center'>TIMEZONE</h3>
-              <span className='text-lg font-bold text-[#2b2b2b]'>{location.timezone}</span>
+              <span className='text-lg font-bold text-[#2b2b2b]'>{ipAddress?.location.timezone}</span>
             </div>
 
             <div>
               <h3 className='text-xs text-[#969696] text-center'>ISP</h3>
-              <span className='text-lg font-bold text-[#2b2b2b]'>{isp}</span>
+              <span className='text-lg font-bold text-[#2b2b2b]'>{ipAddress?.isp}</span>
             </div>
           </>
         )
